95be36c92400016ea0b573539828abb2677cb2a6
Djorgiii <jorgegoncalves1200@gmail.com>
2025-09-22 14:54:25 +0100

    Adicionei o codigo completo do trabalho 1

---
+++ FSO/Trabalho1/CarrinhoApp/src/BaseDados.java
@@ -0,0 +1,59 @@
+
+public class BaseDados {
+    private boolean terminar;
+    private RobotLegoEV3 robot;
+    private boolean robotAberto;
+    private int distancia;
+    private int angulo;
+    private int raio;
+
+    public int getDistancia() {
+		return distancia;
+	}
+
+	public int getAngulo() {
+		return angulo;
+	}
+
+	public void setAngulo(int angulo) {
+		this.angulo = angulo;
+	}
+
+	public int getRaio() {
+		return raio;
+	}
+
+	public void setRaio(int raio) {
+		this.raio = raio;
+	}
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+
+	public boolean isRobotAberto() {
+        return robotAberto;
+    }
+
+    public void setRobotAberto(boolean robotAberto) {
+        this.robotAberto = robotAberto;
+    }
+
+    public BaseDados() {
+        robot = new RobotLegoEV3();
+        terminar = false;
+        robotAberto = false;
+    }
+
+    public RobotLegoEV3 getRobot() {
+        return robot;
+    }
+
+    public boolean isTerminar() {
+        return terminar;
+    }
+
+    public void setTerminar(boolean terminar) {
+        this.terminar = terminar;
+    }
+}
