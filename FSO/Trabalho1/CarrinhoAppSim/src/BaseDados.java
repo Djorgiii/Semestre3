9ff48b4e70657e5813cbcc0ff739e676fc18846b
msca0431 <marsocodrade@gmail.com>
2025-09-22 15:59:34 +0100

    Adicionei o robot simulado
    +classe robotsim
    +mudanças na base de dados para adaptar ao sim

---
+++ FSO/Trabalho1/CarrinhoAppSim/src/BaseDados.java
@@ -0,0 +1,59 @@
+
+public class BaseDados {
+    private boolean terminar;
+    private RobotLegoEV3Sim robot;
+    private boolean robotAberto;
+    private int distancia;
+    private int raio;
+    private int angulo;
+
+    public int getRaio() {
+		return raio;
+	}
+
+	public void setRaio(int raio) {
+		this.raio = raio;
+	}
+
+	public int getAngulo() {
+		return angulo;
+	}
+
+	public void setAngulo(int angulo) {
+		this.angulo = angulo;
+	}
+
+	public int getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+
+	public boolean isRobotAberto() {
+        return robotAberto;
+    }
+
+    public void setRobotAberto(boolean robotAberto) {
+        this.robotAberto = robotAberto;
+    }
+
+    public BaseDados() {
+        robot = new RobotLegoEV3Sim("António");
+        terminar = false;
+        robotAberto = false;
+    }
+
+    public RobotLegoEV3Sim getRobot() {
+        return robot;
+    }
+
+    public boolean isTerminar() {
+        return terminar;
+    }
+
+    public void setTerminar(boolean terminar) {
+        this.terminar = terminar;
+    }
+}
