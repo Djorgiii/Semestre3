0b77676768cd75a6cdf1563d81323034e35ee82a
Djorgiii <jorgegoncalves1200@gmail.com>
2025-09-23 17:45:47 +0100

    Update ComandosAleatorios.java

--- FSO/Trabalho1/CarrinhoApp/src/ComandosAleatorios.java
+++ FSO/Trabalho1/CarrinhoApp/src/ComandosAleatorios.java
@@ -4,7 +4,7 @@ public class ComandosAleatorios {
 	//Metodo gerar comandos
 	//Reta entre 10 e 50cm.
 	//Curvas raio entre 10 e 30cm e angulo de 20 a 90 graus
-	//Parar sempre False
+	//Parar sempre False.
 	
 	public void GerarComando(int numComandos) {
 		
