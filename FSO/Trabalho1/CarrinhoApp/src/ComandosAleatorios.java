e4f411eb22c4de2ea396f537648a358f75b118d5
Djorgiii <jorgegoncalves1200@gmail.com>
2025-10-15 00:27:38 +0100

    Alterações FSO

--- FSO/Trabalho1/CarrinhoApp/src/ComandosAleatorios.java
+++ FSO/Trabalho1/CarrinhoApp/src/ComandosAleatorios.java
@@ -1,13 +1,11 @@
-
 public class ComandosAleatorios extends Tarefa {
 	private BufferCircular buffercircular;
 	
-	public ComandosAleatorios(Tarefa t) {
+	public ComandosAleatorios(BufferCircular buffercircular, Tarefa t) {
 		super(t);
+		this.buffercircular = buffercircular;
 	}
 	
-	
-
 	public void execucao() {
 	    String[] tipos = {"PARAR", "RETA", "CURVARDIREITA", "CURVARESQUERDA"};
 	    for (int i = 0; i < 5; i++) {
@@ -15,10 +13,13 @@ public class ComandosAleatorios extends Tarefa {
 	        Comando comando;
 	        if (tipo.equals("PARAR")) {
 	            comando = new Comando(tipo, false); // Exemplo: PARAR pode usar o construtor com boolean
+	        } else if (tipo.equals("RETA")) {
+	            int distancia = 10 + (int)(Math.random() * 41); // 10 a 50
+	            comando = new Comando(tipo, distancia, 0);
 	        } else {
-	            int arg1 = (int)(Math.random() * 100);
-	            int arg2 = (int)(Math.random() * 100);
-	            comando = new Comando(tipo, arg1, arg2);
+	            int raio = 10 + (int)(Math.random() * 21); // 10 a 30
+	            int angulo = 20 + (int)(Math.random() * 71); // 20 a 90
+	            comando = new Comando(tipo, angulo, raio);
 	        }
 	        buffercircular.inserirElemento(comando);
 	        dormir(); // Simula tempo de produção
--- FSO/Trabalho1/CarrinhoApp/src/Servidor.java
+++ FSO/Trabalho1/CarrinhoApp/src/Servidor.java
@@ -1,22 +1,52 @@
-
 public class Servidor extends Thread{
 	private BufferCircular buffercircular;
 	private RobotLegoEV3 asdrubal;
 	
 	
+	public Servidor(BufferCircular buffercircular, RobotLegoEV3 asdrubal) {
+	    this.buffercircular = buffercircular;
+	    this.asdrubal = asdrubal;
+	}
+	
 	public void Reta(int distancia) {
-		buffercircular.removerElemento();
+		asdrubal.Reta(distancia);
 	}
 	
 	public void CurvarDireita(int distancia, int raio) {
-		buffercircular.removerElemento(new Comando("CURVARDIREITA", distancia, raio));
+		asdrubal.CurvarDireita(distancia, raio);
 	}
 	
 	public void CurvarEsquerda(int distancia, int raio) {
-		buffercircular.removerElemento(new Comando("CURVARESQUERDA", distancia, raio));
+		asdrubal.CurvarEsquerda(distancia, raio);
 	}
 	
 	public void Parar(boolean b) {
-		buffercircular.removerElemento(new Comando("PARAR", b));
+		asdrubal.Parar(b);
+	}
+
+	
+	public void execucao() {
+	    while (true) {
+	        Comando comando = buffercircular.removerElemento();
+	        if (comando != null) {
+	            switch (comando.getTipo()) {
+	                case "reta":
+	                    Reta(comando.getArg1());
+	                    break;
+	                case "curvarDireita":
+	                    CurvarDireita(comando.getArg1(), comando.getArg2());
+	                    break;
+	                case "curvarEsquerda":
+	                    CurvarEsquerda(comando.getArg1(), comando.getArg2());
+	                    break;
+	                case "parar":
+	                    Parar(true);
+	                    break;
+	                default:
+	                    // Comando desconhecido
+	                    break;
+	            }
+	        }
+	    }
 	}
 }
